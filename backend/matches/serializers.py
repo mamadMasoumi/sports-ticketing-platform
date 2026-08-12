3d5663ffc89d8fcaaf8983d9502bed879ed4df5c
mamad <mmd.razer84@gmail.com>
2026-08-12 16:02:52 +0330

    Update serializers.py

--- backend/matches/serializers.py
+++ backend/matches/serializers.py
@@ -1,4 +1,9 @@
 from rest_framework import serializers
 
+
+class ReserveTicketSerializer(serializers.Serializer):
+    ticket_id = serializers.IntegerField(min_value=1)
+
+
 class CancelReservationSerializer(serializers.Serializer):
-    reservation_id = serializers.IntegerField()
+    reservation_id = serializers.IntegerField(min_value=1)
